b868259672f581dc60a8902e1f50247b47b0f416
Ethan Dye <mrtops03@gmail.com>
2018-02-27 18:46:03 -0500

    Add Python and JavaScript examples

---
+++ JavaScript/PigLatinTranslator.js
@@ -0,0 +1,35 @@
+console.log('Welcome to Pig Latin Translator!');
+
+function PigTrans() {
+    /* Translates words into Pig Latin */
+    var pyg = 'ay';
+    var original = prompt('Enter a word:');
+    if (original.length > 0 && isNaN(original)) {
+        var word = original.toLowerCase();
+        var first = word.charAt(0);
+        var second = word.charAt(1).toUpperCase();
+        var newWord = second + word.slice(2) + first + pyg;
+        console.log(newWord);
+    } else {
+        console.log('Not a valid word');
+    }
+}
+
+function ContCheck() {
+    /* Checks if python should run PigTrans again */
+    var runAgain = prompt("Please type 'continue' to translate another word or type 'exit' to close:");
+    runAgain.toLowerCase();
+    
+    if (runAgain == 'continue') {
+        PigTrans();
+        ContCheck();
+    } else if (runAgain == 'exit') {
+        return;
+    } else {
+        console.log('Please choose to either continue or exit');
+        ContCheck();
+    }
+}
+
+PigTrans();
+ContCheck();
---
+++ Python/PigLatinTranslator.py
@@ -0,0 +1,37 @@
+﻿print ("Welcome to Pig Latin Translator!")
+
+
+def PigTrans():
+    """Translates words into Pig Latin"""
+    pyg = "ay"
+    original = input("Enter a word:")
+
+    if len(original) > 0 and original.isalpha():
+        word = original.lower()
+        first = word[0]
+        second = word[1].upper()
+        new_word = second + word[2:len(word)] + first + pyg
+        print (new_word)
+    else:
+        print ("Not a valid word")
+
+
+def ContCheck():
+    """Checks if python should run PigTrans again"""
+    runAgain = input("""
+Please type 'continue'
+to translate another word
+or type 'exit' to close:
+""").lower()
+
+    if runAgain == "continue":
+        PigTrans()
+        ContCheck()
+    elif runAgain == "exit":
+        exit()
+    else:
+        print("Please choose to either continue or exit")
+        ContCheck()
+
+PigTrans()
+ContCheck()
